ed608bbc66932dff84cc0b5653ebad4fad2da775
php 11 <php.11@lampadinha.cec.ime.usp.br>
2018-01-19 21:05:23 -0200

    pares e impares

--- logica2.php
+++ logica2.php
@@ -1,13 +1,15 @@
 <?php
 
+
+
 for ($i=1; $i<101 ; $i++)
 {
-  if ($i % 4 === 0)
+  if ($i % 2 === 0)
   {
-    echo "PIN" .PHP_EOL;
+    echo "**" .PHP_EOL;
   }
   else{
-    echo "$i" .PHP_EOL;
+    echo "*" .PHP_EOL;
   }
 }
 ?>
